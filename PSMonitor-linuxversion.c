bfbad905e8e0774c857c130d2645c3d559e2d8a7
ahmedmalii <ahmed195131@feng.bu.edu.eg>
2022-12-22 09:31:49 -0500

    try

--- PSMonitor-linuxversion.c
+++ PSMonitor-linuxversion.c
@@ -7,7 +7,10 @@
 #include <string.h>
 #include <errno.h>
 //  edited
+const int MAX=1000;
+void getCurrentRunningProcess(){
    const char* path="/proc";
+    DIR *folder;
     int files=0; // num of files in proc
     struct dirent *entry; // directory entry to read entries in  from dir
     folder=opendir(path);
